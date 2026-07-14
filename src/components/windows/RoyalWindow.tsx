987b57cc1f93514748701942eb106bcb6aabb453
Darius <120105848+DumitrescuDarius@users.noreply.github.com>
2026-07-14 21:37:31 +0300

    little effect bug fix

--- src/components/windows/RoyalWindow.tsx
+++ src/components/windows/RoyalWindow.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Crown, Check, ShieldCheck, Loader2, CreditCard, Lock, AlertTriangle } from "lucide-react";
 import { WindowSpinner } from "../WindowSpinner";
 import { TranslationKey } from "../../constants/translations";
